854c29fac2994eafdd8ca6af01c48ca5aee708b7
Ben <benjamin_an@yahoo.com>
2025-02-10 15:05:44 -0500

    Update mod.rs
    Signed-off-by: Ben <benjamin_an@yahoo.com>

--- pg_search/src/postgres/customscan/pdbscan/exec_methods/fast_fields/mod.rs
+++ pg_search/src/postgres/customscan/pdbscan/exec_methods/fast_fields/mod.rs
@@ -200,9 +200,17 @@ pub unsafe fn pullup_fast_fields(
                 }
 
                 attno => {
-                    let att = tupdesc
-                        .get((attno - 1) as usize)
-                        .expect("attnum should exist in tupdesc");
+                    let att = match tupdesc.get((attno - 1) as usize) {
+                        Some(att) => att,
+                        None => {
+                            ereport!(
+                                pg_sys::WARNING,
+                                message = format!("Attribute {} not found in tuple descriptor", attno)
+                            );
+                            return None;
+                        }
+                    };
+
                     if schema.is_fast_field(att.name()) {
                         let ff_type = if (*var).vartype == pg_sys::TEXTOID
                             || (*var).vartype == pg_sys::VARCHAROID
